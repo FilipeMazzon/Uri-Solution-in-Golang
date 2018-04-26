adb45bb7d56de5cb6cdc1c3689807437e52e87c3
filipe mazzon ribeiro <filipe.mazzon@liveware.com.br>
2018-04-26 16:03:41 -0300

    first exercises

---
+++ exercicio1002.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main()  {
+	var Raio float64
+	fmt.Scan(&Raio)
+	const PI = 3.14159
+	VOLUME := PI * math.Pow(Raio,2)
+	fmt.Printf("A=%.4f\n",VOLUME)
+}
---
+++ exercicio1011.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var Raio float64
+	fmt.Scan(&Raio)
+	const PI = 3.14159
+	VOLUME := 4.0 / 3 * PI * math.Pow(Raio,3)
+	fmt.Printf("VOLUME = %.3f\n",VOLUME)
+
+}
